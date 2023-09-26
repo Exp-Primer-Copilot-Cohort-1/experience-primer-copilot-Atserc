4489aadbc0ee01242833282afe4517cc4b1761b9
Lisandro Cresta <75375864+Atserc@users.noreply.github.com>
2023-09-26 18:40:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,65 +1,100 @@
 // Create web server
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("./config");
+const Comment = require("./models/Comment");
+const User = require("./models/User");
 const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Create posts object
-const posts = {};
+// Connect to MongoDB database
+mongoose.connect(config.db, { useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
-// Handle post requests to /posts
-app.post('/posts', async (req, res) => {
-  // Get data from request body
-  const { title } = req.body;
-
-  // Create id for post
-  const id = Math.random().toString(36).substring(2);
-
-  // Add post to posts object
-  posts[id] = { id, title, comments: [] };
-
-  // Send data back to client
-  await axios.post('http://event-bus-srv:4005/events', {
-    type: 'PostCreated',
-    data: { id, title },
+// Get all comments from database
+app.get("/", (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comments);
+    }
   });
-
-  // Send response to client
-  res.status(201).send(posts[id]);
 });
 
-// Handle get requests to /posts
-app.get('/posts', (req, res) => {
-  // Send posts object back to client
-  res.send(posts);
+// Get all comments from database
+app.get("/comments", (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(comments);
+    }
+  });
 });
 
-// Handle post requests to /events
-app.post('/events', (req, res) => {
-  // Get event from request body
-  const { type, data } = req.body;
-
-  // Check if event is comment created
-  if (type === 'CommentCreated') {
-    // Get comment from event data
-    const { id, content, postId } = data;
-
-    // Get post from posts object
-    const post = posts[postId];
-
-    // Add comment to post
-    post.comments.push({ id, content });
-  }
-
-  // Send response to client
-  res.send({});
+// Get comment by id
+app.get("/comments/:id", (req, res) => {
+  let id = req.params.id;
+  Comment.findById(id, (err, comment) => {
+    res.json(comment);
+  });
 });
 
-// Listen on port 4002
-app.listen(4002, () => {
-  console.log('Listening on 4002');
+// Get comments by user id
+app.get("/user/:id", (req, res) => {
+  let id = req.params.id;
+  Comment.find({ user_id: id }, (err, comments) => {
+    res.json(comments);
+  });
+});
+
+// Get comments by post id
+app.get("/post/:id", (req, res) => {
+  let id = req.params.id;
+  Comment.find({ post_id: id }, (err, comments) => {
+    res.json(comments);
+  });
+});
+
+// Add new comment to database
+app.post("/add", (req, res) => {
+  let comment = new Comment(req.body);
+  comment
+    .save()
+    .then(comment => {
+      res.status(200).json({ comment: "comment added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new comment failed");
+    });
+});
+
+// Update comment by id
+app.post("/update/:id", (req, res) => { 
+  Comment.findById(req.params.id, (err, comment) => {
+    if (!comment) res.status(404).send("data is not found");
+    else {
+      comment.comment = req.body.comment;
+      comment
+        .save()
+        .then(comment => {
+          res.json("Comment updated!");
+        })
+        .catch(err => {
+          res.status(400).send("Update not possible");
+        });
+    }
+  });
 });
